755fb1963f66695db37aa23c35f8152be5bcead6
ergot <ergot@users.noreply.github.com>
2022-01-24 10:47:12 +0100

    Getting list of crew

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
+const urlCrew = 'https://api.spacexdata.com/v4/crew'
+
+const CardUser = ({ user, handleClick }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <p
+      onClick={() => {
+        handleClick(user.id)
+      }}
+    >
+      {user.name}
+    </p>
+  )
 }
 
-export default App;
+function App() {
+  const [users, setUsers] = useState(null)
+
+  // Récupération des datas au montage
+  useEffect(() => {
+    fetch(urlCrew)
+      .then((res) => res.json())
+      .then((result) => setUsers(result))
+  }, [])
+
+  const handleClick = (id) => {
+    //setIdProfil(id);
+    console.log('click', id)
+  }
+
+  const displayUsers = (users) => {
+    if (users === null) {
+      return 'loading'
+    } else {
+      return users.map((user, i) => {
+        return <CardUser key={i} user={user} handleClick={handleClick} />
+      })
+    }
+  }
+
+  return (
+    <div>
+      <section>
+        <h2>Liste de tripulants</h2>
+        {displayUsers(users)}
+      </section>
+      <section>
+        <h2>Details du membre</h2>
+        {/* {displayDetail()} */}
+      </section>
+    </div>
+  )
+}
+
+export default App
